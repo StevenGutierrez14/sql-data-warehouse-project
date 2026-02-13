3e58fdcf59d40c317fe2d9d1eceb887df5641c56
Steven Gutiérez <167136908+StevenGutierrez14@users.noreply.github.com>
2026-02-12 20:50:22 -0600

    Add SQL script for dimensions exploration

---
+++ scripts/Exploratory Data Analysis/02_dimensions_exploration.sql
@@ -0,0 +1,9 @@
+--Explore all countries our customers come from
+
+SELECT DISTINCT country FROM gold.dim_customers 
+
+
+--Explore All Product Categories "The major Divisions"
+
+SELECT DISTINCT category, subcategory, product_name FROM gold.dim_products
+ORDER BY 1, 2, 3
